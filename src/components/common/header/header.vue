6554bc42962275c07b06501fcfa662d4f645915e
박상민 <a01071397193@gmail.com>
2025-04-14 15:31:52 +0900

    feat :: dark mode

--- src/components/common/header/header.vue
+++ src/components/common/header/header.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
 import Logo from "@/assets/images/Logo.svg";
+import Light from "@/assets/images/header/light.svg";
+import Dark from "@/assets/images/header/dark.svg";
+import { useThemeStore } from "@/stores/theme/theme.store";
+
+const themeStore = useThemeStore();
 </script>
 
 <template>
   <header>
     <div class="logo_container">
-      <Logo/>
+      <Logo />
       <div class="menu_container">
         <span>포트폴리오</span>
         <span>면접</span>
       </div>
     </div>
-    <div>
-
+    <div class="theme_toggle" @click="themeStore.toggleTheme">
+      <div class="icon_wrapper">
+        <transition name="fade-icon" mode="out-in">
+          <Light v-if="themeStore.theme === 'light'" key="light" class="icon" />
+          <Dark v-else key="dark" class="icon" />
+        </transition>
+      </div>
     </div>
   </header>
-
 </template>
 
 <style scoped lang="scss">
@@ -49,4 +58,28 @@ header {
   }
 }
 
+.theme_toggle {
+  cursor: pointer;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: transform 0.02s ease;
+
+  &:active {
+    transform: scale(0.9);
+  }
+}
+
+.fade-icon-enter-active,
+.fade-icon-leave-active {
+  transition: opacity 0.1s ease, transform 0.1s ease;
+}
+
+.fade-icon-enter-from,
+.fade-icon-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { createApp } from 'vue'
-import {createPinia} from "pinia";
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 import router from "@/router";
+import { useThemeStore } from "@/stores/theme/theme.store";
 
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
 app.use(router);
-app.mount('#app');
+
+const themeStore = useThemeStore();
+themeStore.initTheme();
+
+app.mount("#app");
