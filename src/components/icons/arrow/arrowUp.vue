3dc59429d781d776068ce170a6401b7c85e099b8
developersihyeon <sihyeonsky@gmail.com>
2025-04-14 15:58:10 +0900

    feat :: Icon 컴포넌트 생성 #2

---
+++ src/components/icons/arrow/arrowUp.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  size?: number;
+  color?: string;
+}
+
+const { size = 24, color = "var(--Static-Black)" } = defineProps<Props>();
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M17 14L12 9L7 14"
+      :stroke="color"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round" />
+  </svg>
+</template>
---
+++ src/views/auth/signup.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Apply from "@/components/icons/apply.vue";
+import ArrowUp from "@/components/icons/arrow/arrowUp.vue";
+import ArrowDown from "@/components/icons/arrow/arrowDown.vue";
+import Bold from "@/components/icons/bold.vue";
+import Check from "@/components/icons/check.vue";
+import Download from "@/components/icons/download.vue";
+import Error from "@/components/icons/error.vue";
+import BulletedList from "@/components/icons/list/bulletedList.vue";
+import NumberList from "@/components/icons/list/numberList.vue";
+import Pen from "@/components/icons/pen.vue";
+import Plus from "@/components/icons/plus.vue";
+import Save from "@/components/icons/save.vue";
+import Search from "@/components/icons/search.vue";
+import Sparkles from "@/components/icons/sparkles.vue";
+import Trash from "@/components/icons/trash.vue";
+import Xmark from "@/components/icons/xmark.vue";
+</script>
+
+<template>
+  <h1>회원가입</h1>
+  <Apply color="#000" />
+  <ArrowUp color="#000" />
+  <ArrowDown color="#000" />
+  <Bold color="#000" />
+  <Check color="#000" />
+  <Download color="#000" />
+  <Error color="#000" />
+  <BulletedList color="#000" />
+  <NumberList color="#000" />
+  <Pen color="#000" />
+  <Plus color="#000" />
+  <Save color="#000" />
+  <Search color="#000" />
+  <Sparkles color="#000" />
+  <Trash color="#000" />
+  <Xmark color="#000" />
+</template>
