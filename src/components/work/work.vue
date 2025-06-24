541072c9a69f66f4d57ad5b58b087860ad9c638d
박상민 <a01071397193@gmail.com>
2025-06-24 09:29:10 +0900

    feat :: Work

---
+++ src/components/work/work.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import styles from "./style.module.scss";
+import FilledTextField from "@/components/ui/textFeield/filledTextField/filledTextField.vue";
+const work = ref("");
+</script>
+
+<template>
+<div :class="styles.container">
+  <p>직무 · 직군</p>
+  <FilledTextField v-model="work" type="text" placeholder="직무 · 직군을 입력해주세요" :isLabel="false" />
+</div>
+</template>
