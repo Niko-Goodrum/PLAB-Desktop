a428889f21c333aa1fea489960b29e88baeef268
박상민 <a01071397193@gmail.com>
2025-06-26 12:06:16 +0900

    chore :: auth

--- src/libs/axios/customAxios.ts
+++ src/libs/axios/customAxios.ts
@@ -1,4 +1,3 @@
-import config from "@/config/config.json";
 import axios, { AxiosRequestConfig } from "axios";
 import token from "../token/token";
 import {
--- src/repository/auth/auth.repository.ts
+++ src/repository/auth/auth.repository.ts
@@ -3,32 +3,27 @@ import { Response } from "@/types/util/response";
 import { SigninResponse, RefreshResponse } from "@/types/auth/auth.type";
 import { SignupParams, SigninParams, RefreshParams } from "./auth.param";
 
-const Plab_Server = import.meta.env.VITE_Plab_Server;
+const SERVER_URL = import.meta.env.VITE_Plab_Server;
 
 class AuthRepository {
-  public async postSignup(params: SignupParams): Promise<Response> {
-    const { data } = await axios.post(
-      `${Plab_Server}/auth/signup`,
-      params
-    );
-    return data;
-  }
+    public async postSignup(params: SignupParams): Promise<Response> {
+        const { data } = await axios.post(`${SERVER_URL}/auth/signup`, params);
 
-  public async postSignin(params: SigninParams): Promise<SigninResponse> {
-    const { data } = await axios.post(
-      `${Plab_Server}/auth/signin`,
-      params
-    );
-    return data;
-  }
+        return data;
+    }
 
-  public async postRefreshToken(params: RefreshParams): Promise<RefreshResponse> {
-    const { data } = await axios.post(
-      `${Plab_Server}/auth/refresh`,
-      params
-    );
-    return data;
-  }
+    public async postSignin(params: SigninParams): Promise<SigninResponse> {
+        const { data } = await axios.post(`${SERVER_URL}/auth/signin`, params);
+
+        return data;
+    }
+
+    public async postRefreshToken(
+        params: RefreshParams
+    ): Promise<RefreshResponse> {
+        const { data } = await axios.post(`${SERVER_URL}/auth/refresh`, params);
+        return data;
+    }
 }
 
 const authRepository = new AuthRepository();
